495e0c4b76a5caca961843d19cbb245822c9e2ae
Ridhiwan <ridhiwanmseya@gmail.com>
2023-05-23 10:07:28 +0300

    homepage framework

--- lib/main.dart
+++ lib/main.dart
@@ -7,39 +7,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'DaybyDay',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepOrange,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'DaybyDay'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -52,57 +33,133 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Center(child: Text(widget.title,textAlign: TextAlign.center)),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: Colors.grey.shade300,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  width: 450,
+                  height: 100,
+                  padding: const EdgeInsets.symmetric(vertical: 7,horizontal: 7),
+                  child: Center(
+                      child: Text(
+                        "How are you feeling right now?",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w700,
+                          fontStyle: FontStyle.italic,
+                          color: const Color.fromRGBO(200, 100, 100, 1),
+                          shadows: [
+                            const Shadow(
+                              color: Color.fromRGBO(10, 200, 150, 1),
+                              blurRadius: 10.0,
+                              offset: Offset(5.0, 5.0),
+                            ),
+                            Shadow(
+                              color: Colors.yellow.shade300,
+                              blurRadius: 5.0,
+                              offset: const Offset(-5.0, 5.0),
+                            ),
+                          ],
+                        )
+
+                        ),)
+                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 190,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16),
+                          color: Colors.yellow.shade100,
+                          boxShadow: [
+                            const BoxShadow(
+                                offset: Offset(10, 10),
+                                color: Colors.black38,
+                                blurRadius: 20),
+                            BoxShadow(
+                                offset: const Offset(-10, -10),
+                                color: Colors.white.withOpacity(0.85),
+                                blurRadius: 20)
+                          ]),
+                      child: Center(
+                        child: Text(
+                          'HAPPY!',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 35,
+                              shadows: [
+                                const Shadow(
+                                    offset: Offset(3, 3),
+                                    color: Colors.black38,
+                                    blurRadius: 10),
+                                Shadow(
+                                    offset: const Offset(-3, -3),
+                                    color: Colors.white.withOpacity(0.85),
+                                    blurRadius: 10)
+                              ],
+                              color: Colors.yellow),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20,width: 12,),
+                    Container(
+                      height: 100,
+                      width: 190,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16),
+                          color: Colors.brown.shade100,
+                          boxShadow: [
+                            const BoxShadow(
+                                offset: Offset(10, 10),
+                                color: Colors.black38,
+                                blurRadius: 20),
+                            BoxShadow(
+                                offset: const Offset(-10, -10),
+                                color: Colors.white.withOpacity(0.85),
+                                blurRadius: 20)
+                          ]),
+                      child: Center(
+                        child: Text(
+                          'SAD:(',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 35,
+                              shadows: [
+                                const Shadow(
+                                    offset: Offset(3, 3),
+                                    color: Colors.black38,
+                                    blurRadius: 10),
+                                Shadow(
+                                    offset: const Offset(-3, -3),
+                                    color: Colors.white.withOpacity(0.85),
+                                    blurRadius: 10)
+                              ],
+                              color: Colors.brown),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
